cb9e0e08fb8d2644b1292d4a82a4ff734d997afe
Maria Guo <68859666+MariaG12137@users.noreply.github.com>
2020-08-30 13:02:35 -0400

    Update Array - 2Sums - Input array is sorted.js

--- Array - 2Sums - Input array is sorted.js
+++ Array - 2Sums - Input array is sorted.js
@@ -18,3 +18,31 @@ var twoSum = function(numbers, target) {
     }
     return false;
 };
+===================================================================================================================================================
+/*
+Array is not sorted
+*/
+var twoSum = function(nums, target) {
+    let map = new Map();
+    for(i=0; i<nums.length;i++){
+        if(!map.has(nums[i])){
+            map.set(nums[i],[i])
+        }else{
+            map.get(nums[i]).push(i)
+        }
+    }
+    nums.sort((a,b)=>a-b);
+    let l=0;
+    let r=nums.length-1;
+    
+    while(l<r){
+        if(nums[l]+nums[r]==target){
+            return [map.get(nums[l]).pop(),map.get(nums[r]).pop()]
+        } else if(nums[l]+nums[r]<target){
+            l++;
+        }else{
+            r--;
+        }
+    }
+};
+    
